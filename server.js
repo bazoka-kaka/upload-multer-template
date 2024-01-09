0c4fc5b0f281ba32bb22a31ed73c20d7be6dfd29
bazoka-kaka <yehezkielwiradhika@gmail.com>
2024-01-10 05:15:00 +0700

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const upload = require("./middleware/upload");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 // middlewares
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
